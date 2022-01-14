2975d99adb2ce7d4cff536db9a316445c23e5909
AL IMRAN <imu.png@gmail.com>
2022-01-15 04:51:46 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+    "use strict";
+
+
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 600,
+            modifier: 1,
+            slideShadows: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+
+
+
+
+}(jQuery));
